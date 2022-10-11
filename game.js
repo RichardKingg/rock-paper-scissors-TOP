21d06c7faa390e1df994c7416c3b509c67b9c955
RichardKingg <ricardo.elizondo.jau@gmail.com>
2022-10-11 00:24:56 -0500

    Add function playRound

--- game.js
+++ game.js
@@ -4,3 +4,25 @@ function getComputerChoice() {
   let rando = Math.floor(Math.random() * arrGame.length);
   return rando, arrGame[rando];
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection == "Rock" && computerSelection == "Rock") ||
+    (playerSelection == "Paper" && computerSelection == "Paper") ||
+    (playerSelection == "Scissors" && computerSelection == "Scissors")
+  ) {
+    return "Draw!";
+  } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+    return "You lose, paper beats rock!";
+  } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    return "You lose, scissors beat paper!";
+  } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    return "You lose, rock beats scissors!";
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    return "You win, rock beats scissors!";
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    return "You win, paper beats rock!";
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    return "You win, scissors beat paper!";
+  }
+}
